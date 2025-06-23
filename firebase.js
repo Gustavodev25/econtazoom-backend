9f22a55f6ae84f0805341582ce8ab368bcbf6fbd
Gustavo25 <59091992+Gustavodev25@users.noreply.github.com>
2025-06-23 12:13:50 -0300

    nova

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./econtazoom-9c5d8-firebase-adminsdk-fbsvc-9a8bf02632.json'); 
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
-const db = admin.firestore();
-module.exports = { db };
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
